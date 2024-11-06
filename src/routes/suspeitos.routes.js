f52c1c05de180294f50a3977b0a251a28c86b373
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2024-11-06 20:50:13 -0300

    feat:create post method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -3,13 +3,6 @@ import { Router } from "express";
 const suspeitosRoutes = Router();
 
 let suspeitos = [
-    {
-        id: Math.floor(Math.random() * 100),
-        nome: "Vinicius Valverde",
-        profissao: "Jogador",
-        envolvimento: "sim",
-        nivelSuspeita: "baixo"
-    }
 ];
 
 //Rota para buscar todos os supeitos;
@@ -30,6 +23,46 @@ suspeitosRoutes.get("/:id", (req, res) => {
 
     return res.status(200).send ({
         message: `O suspeito com id ${id} é o:`,suspeito
+    });
+});
+
+//Rota para cadastrar um novo suspeito;
+suspeitosRoutes.post("/", (req ,res) => {
+    const { nome, profissao, envolvimento, nivelSuspeita } = req.body;
+
+    //Validação;
+    if ( !nome || !profissao || !nivelSuspeita ) {
+        return res.status(400).send({
+            message: "Os campos de nome, profissão e nivelSuspeita devem ser preenchidos!"
+        });
+    };
+
+    //Campo de nível de suspeita;     "toLowerCase = se for minusuclo ou maiusuclo ainda vai funcionar"
+    if ( nivelSuspeita.toLowerCase () != "baixo" && nivelSuspeita.toLowerCase() != "médio" && nivelSuspeita.toLowerCase () != "alto" ) {
+        return res.status(400).send ({
+            message: " O campo de nível de suspeita deve ser preenchido com 'baixo', 'médio' ou 'alto'. "
+        });
+    }
+    //Validação envolvimento;
+    if (envolvimento.toLowerCase () != "sim" && envolvimento.toLowerCase() != "não"){
+        return res.status(400).send ({
+            message: "O campo de envolvimento deve ser preenchido com 'sim' ou 'não'."
+        });
+    };
+
+    const novoSuspeito = {
+        id: Math.floor(Math.random() * 100),
+        nome,
+        profissao,
+        envolvimento,
+        nivelSuspeita
+    };
+
+    suspeitos.push(novoSuspeito);
+
+    return res.status(200).send ({
+        message: "Suspeito cadastrado com sucesso",
+        novoSuspeito,
     });
 });
 
